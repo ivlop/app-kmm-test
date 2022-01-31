82383c00707d9bf120de3f15537a9590112cc211
Iván López Sancosmed <ivan.lopez.sancosmed@udc.es>
2022-01-31 17:21:09 +0100

    delete build folders

--- build/ios/Pods.build/Debug-iphonesimulator/Pods-iosApp.build/Objects-normal/x86_64/Pods-iosApp-dummy.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/ivan/Desktop/pruebakmm/iosApp/Pods/Target\ Support\ Files/Pods-iosApp/Pods-iosApp-dummy.m \
-  /Users/ivan/Desktop/pruebakmm/build/ios/Pods.build/Debug-iphonesimulator/Pods-iosApp.build/module.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator14.5.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator14.5.sdk/usr/include/mach-o/dyld.modulemap
--- build/ios/Pods.build/Debug-iphonesimulator/Pods-iosApp.build/Objects-normal/x86_64/Pods_iosApp_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/ivan/Desktop/pruebakmm/build/ios/Pods.build/Debug-iphonesimulator/Pods-iosApp.build/DerivedSources/Pods_iosApp_vers.c
